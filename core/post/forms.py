0d1759009fee04826ba750cd08d4dec7ce5deaeb
ALi <a_anvari@ymail.com>
2025-12-15 14:21:28 +0330

    add blog post model to post app

--- core/post/forms.py
+++ core/post/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Task
+from blog.models import Post
 
 
 class TaskForm(forms.ModelForm):
@@ -7,6 +8,24 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["author","title"]
+        widgets = {
+            "author": forms.HiddenInput(),
+        }
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'author',
+            'image',
+            'title',
+            'content',
+            'tags',
+            'category',
+            'status',
+            'login_require',
+        ]
         widgets = {
             "author": forms.HiddenInput(),
         }
