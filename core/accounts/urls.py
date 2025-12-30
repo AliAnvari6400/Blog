d0f49908649bd56d0cbc634023182343cd290688
ALi <a_anvari@ymail.com>
2025-12-30 17:54:38 +0330

    redirect to blog instead redirectview to prevent 302

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -3,11 +3,13 @@ from django.contrib.auth.views import LogoutView
 from .views import SignUpView
 from django.views.generic import RedirectView
 from .views import MyLoginView
+from blog.views import BlogHomeView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="blog/", query_string=True, permanent=False)),
+    #path("", RedirectView.as_view(url="blog/", query_string=True, permanent=False)),
+    path("", BlogHomeView.as_view()),
     # path('',include('django.contrib.auth.urls')),
     path("login/", MyLoginView.as_view(), name="login"),
     path(
